93e6f70a67adde5040764aa6fa0593118636f3f8
stanleyyork <stanleyyork@gmail.com>
2015-10-29 17:26:03 -0700

    fixed auto clicking on link for project-icon-

--- main.js
+++ main.js
@@ -1,27 +1,40 @@
 $(document).ready(function(){
 
+	var portfolio_pr = false;
+	var portfolio_ph = false;
+	var portfolio_wr = false;
+
 	$('#project-icon-pr').click(function(){
-		//event.preventDefault();
+		if(portfolio_pr===false){
+			event.preventDefault();
+		}
 		$(this).attr('src', 'http://stanleyyork.com/wp-content/uploads/2015/09/Screen-Shot-2015-09-21-at-10.33.19-AM.png');
 		$(this).css('width', '300px');
 		$(this).css('height', '500px');
 		$('#href-project-icon-pr').attr("href","http://www.clarelegere.com/profiles/1");
+		portfolio_pr = true;
 	});
 
 	$('#project-icon-ph').click(function(){
-		//event.preventDefault();
+		if(portfolio_ph === false){
+			event.preventDefault();
+		}
 		$(this).attr('src', 'http://stanleyyork.com/wp-content/uploads/2015/10/Screen-Shot-2015-10-27-at-5.06.28-PM.png');
 		$(this).css('width', '300px');
 		$(this).css('height', '300px');
 		$('#href-project-icon-ph').attr("href","http://www.instagram.com/stanleyyork/");
+		portfolio_ph = true;
 	});
 
 	$('#project-icon-wr').click(function(){
-		//event.preventDefault();
+		if(portfolio_wr === false){
+			event.preventDefault();
+		}
 		$(this).attr('src', 'http://stanleyyork.com/wp-content/uploads/2015/10/Screen-Shot-2015-10-27-at-5.06.02-PM.png');
 		$(this).css('width', '300px');
 		$(this).css('height', '300px');
 		$('#href-project-icon-wr').attr("href","http://medium.com/@stanleyyork");
+		portfolio_wr = true;
 	});
 
 	$('#about-image-a').mouseenter(function(){
@@ -38,7 +51,7 @@ $(document).ready(function(){
 
 	$(window).scroll(function(){
 
-		if($(window).scrollTop()>50){
+		if($(window).scrollTop()>25){
 			$('body > div.container-photo.container-fluid').fadeOut();
 			$('body > nav > div > div > a > h4').css("color", "black");
 			$('.navbar').css("background-color", "white");
@@ -51,5 +64,6 @@ $(document).ready(function(){
 		}
 
 	});
+
 
 });
